0ce28e24d255a9deecb02efdc5ad62a3ee24288a
thehhe <pecakmaciek@gmail.com>
2018-05-10 00:26:44 +0200

    Moisturize me, moisturize me

--- src/main/java/edgeanalytics/analyzers/MoistureAnalyzer.java
+++ src/main/java/edgeanalytics/analyzers/MoistureAnalyzer.java
@@ -1,8 +1,38 @@
 package edgeanalytics.analyzers;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import edgeanalytics.sensors.MoistureSensor;
+import org.apache.edgent.analytics.math3.json.JsonAnalytics;
 import org.apache.edgent.connectors.iot.IotDevice;
+import org.apache.edgent.connectors.iot.QoS;
+import org.apache.edgent.topology.TStream;
+import org.apache.edgent.topology.TWindow;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.apache.edgent.analytics.math3.stat.Statistic.*;
 
 public class MoistureAnalyzer {
+    private int moisture = 800;
+
     public MoistureAnalyzer(IotDevice device) {
+        TStream<Integer> moistureReadings = device.topology().poll(new MoistureSensor(), 100, TimeUnit.MILLISECONDS);
+
+        moistureReadings = moistureReadings
+                .filter(x -> x < 100)
+                .peek(x -> this.moisture = x);
+
+        TStream<JsonObject> sensorJSON = moistureReadings.map(v -> {
+                    JsonObject j = new JsonObject();
+                    j.addProperty("name", "moistureSensor");
+                    j.addProperty("reading", v);
+                    return j;
+                });
+        TWindow<JsonObject, JsonElement> sensorWindow = sensorJSON.last(10, j -> j.get("name"));
+        sensorJSON = JsonAnalytics.aggregate(sensorWindow, "name", "reading", MIN, MAX, MEAN, STDDEV);
+        //sensorJSON = sensorJSON.filter(j -> j.get("reading").getAsInt() < 100);
+        sensorJSON.print();
+        device.events(sensorJSON, "sensors", QoS.FIRE_AND_FORGET);
     }
 }
--- src/main/java/edgeanalytics/sensors/MoistureSensor.java
+++ src/main/java/edgeanalytics/sensors/MoistureSensor.java
@@ -3,8 +3,22 @@ package edgeanalytics.sensors;
 import org.apache.edgent.function.Supplier;
 
 public class MoistureSensor implements Supplier {
+    private final int minMoisture = 0;
+    private final int maxMoisture = 65536;
+    private int outputMoisture = 800;
+
     @Override
     public Object get() {
-        return null;
+        if(Math.random() > 0.9) {
+            outputMoisture += 1000; //water plant XD
+        } else {
+            int toDecrease = (int)(Math.random() * 100);
+            outputMoisture -= toDecrease;
+        }
+        if(outputMoisture < minMoisture) outputMoisture = minMoisture;
+        if(outputMoisture > maxMoisture) outputMoisture = maxMoisture;
+
+        System.out.println("Current moisture: " + outputMoisture);
+        return outputMoisture;
     }
 }
