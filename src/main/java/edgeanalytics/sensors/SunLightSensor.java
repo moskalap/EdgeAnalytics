cb90c5e43b84fdf471b58cc73a398f8686317539
moskalap <przemyslaw.moskala@gmail.com>
2018-05-10 20:24:33 +0200

    Add temp sensor

--- src/main/java/edgeanalytics/sensors/SunLightSensor.java
+++ src/main/java/edgeanalytics/sensors/SunLightSensor.java
@@ -1,27 +1,67 @@
 package edgeanalytics.sensors;
 
+import com.pi4j.io.i2c.I2CFactory;
 import org.apache.edgent.function.Supplier;
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
+
+import java.io.IOException;
+import java.math.BigDecimal;
 
 public class SunLightSensor implements Supplier {
     private int intensity = 14000;
     private final int maxIntensity = 65535;
     private final int minIntensity = 1;
+    private boolean random;
+    private I2CDevice device;
+
+    SunLightSensor(){
+        I2CBus bus = null;
+        try {
+            bus = I2CFactory.getInstance(I2CBus.BUS_1);
+            device = bus.getDevice(0x23);
+        } catch (I2CFactory.UnsupportedBusNumberException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
     @Override
     public Object get() {
-        if (Math.random() > 0.8) {
-            int change = (int) (Math.random() * 10);
-            if (Math.random() > 0.5)
-                change *= -1;
-            intensity += change;
+        if(this.random){
+            if (Math.random() > 0.8) {
+                int change = (int) (Math.random() * 10);
+                if (Math.random() > 0.5)
+                    change *= -1;
+                intensity += change;
+            }
+
+            if (intensity < minIntensity)
+                intensity = minIntensity;
+            else if (intensity > maxIntensity)
+                intensity = maxIntensity;
+
+            System.out.println("light:\t" + intensity);
+            return intensity;
+        }else{
+            byte[] p = new byte[2];
+
+            int r;
+            try {
+                r = device.read(p, 0, 2);
+            } catch (IOException e) {
+                throw new IllegalStateException(e);
+            }
+
+            if (r != 2) {
+                throw new IllegalStateException("Read Error; r=" + r);
+            }
+            return new BigDecimal((p[0] << 8) | p[1]).intValue();
         }
-
-        if (intensity < minIntensity)
-            intensity = minIntensity;
-        else if (intensity > maxIntensity)
-            intensity = maxIntensity;
-
-        System.out.println("light:\t" + intensity);
-        return intensity;
+        
+    
     }
 }
